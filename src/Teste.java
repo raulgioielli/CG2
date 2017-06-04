6474f4fa58b1359393291d4c08442dbff8be2e8c
Raul Alves da Cunha Silveira Gioielli <raulgioielli@gmail.com>
2017-06-04 01:21:33 -0300

    Alterações
    huehuehue

--- src/Teste.java
+++ src/Teste.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
@@ -11,6 +12,7 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 import org.opencv.photo.Photo;
 
 public class Teste
@@ -22,6 +24,7 @@ public class Teste
 	  Mat imagemProcessar = Imgcodecs.imread("C:\\Users\\raulg\\Desktop\\img2.png", Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
 	  Mat imagemFinal = imagemProcessar.clone();
 	  Mat imagemFinalBW = new Mat(imagemProcessar.rows(),imagemProcessar.cols(),Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+	  Mat imagemCopia = mascara.clone();
 	  
 	  System.out.println(mascara.size());
 	  System.out.println(imagemProcessar.size());
@@ -32,36 +35,52 @@ public class Teste
 	  Imgproc.erode(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,3)));
 	  Imgproc.dilate(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,3)));
 	  
-	  //Imgproc.dilate(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,4)));
-	  //Imgproc.erode(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,4)));
+	  Imgproc.dilate(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,4)));
+	  Imgproc.erode(imagemFinal, imagemFinal, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3,4)));
 	  
 	  Imgproc.cvtColor(imagemFinal, imagemFinalBW, 7);
 	  
 	  Imgproc.findContours(imagemFinalBW, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 	  
+	  List<MatOfPoint> jogadores = new ArrayList<MatOfPoint>();
 	  
-	  MatOfPoint2f approxCurve = new MatOfPoint2f();
+	  for (int i=0; i<contours.size(); i++){
+		  if(Imgproc.contourArea(contours.get(i)) > 20){
+			jogadores.add(contours.get(i));  
+		  }
+		  
+//	        //Convert contours(i) from MatOfPoint to MatOfPoint2f
+//	        MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
+//	        //Processing on mMOP2f1 which is in type MatOfPoint2f
+//	        double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+//	        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+//
+//	        //Convert back to MatOfPoint
+//	        MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
+//
+//	        // Get bounding rect of contour
+//	        Rect rect = Imgproc.boundingRect(points);
+//
+//	         // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
+//	        Imgproc.rectangle(imagemFinal, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(0, 255, 0, 255), 3); 
+
+	  }
 	  
-	  for (int i=0; i<contours.size(); i++)
-	    {
-	        //Convert contours(i) from MatOfPoint to MatOfPoint2f
-	        MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
-	        //Processing on mMOP2f1 which is in type MatOfPoint2f
-	        double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-	        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-
-	        //Convert back to MatOfPoint
-	        MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
-
-	        // Get bounding rect of contour
-	        Rect rect = Imgproc.boundingRect(points);
-
-	         // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-	        Imgproc.rectangle(imagemFinal, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(0, 255, 0, 255), 3); 
-
+	  List<Moments> mu = new ArrayList<Moments>(jogadores.size());
+	    for (int i = 0; i < jogadores.size(); i++) {
+	        mu.add(i, Imgproc.moments(jogadores.get(i), false));
+	        Moments p = mu.get(i);
+	        int x = (int) (p.get_m10() / p.get_m00());
+	        int y = (int) (p.get_m01() / p.get_m00());
+	        
+	        Mat mask = Mat.zeros(imagemFinal.rows(), imagemFinal.cols(), CvType.CV_8U);
+	        Imgproc.drawContours(mask, contours, i, new Scalar(255,255,255), -1);
+	        Scalar mean = Core.mean(imagemFinal, mask);
+	        Imgproc.circle(imagemCopia, new Point(x, y), 4, mean, -1);
+	        
 	    }
 	  
-	  Imgcodecs.imwrite("C:\\Users\\raulg\\Desktop\\img3.png", imagemFinal);
+	  Imgcodecs.imwrite("C:\\Users\\raulg\\Desktop\\img3.png", imagemCopia);
    }
    
    public static void corOriginal(Mat m1, Mat m2, int threshold){
@@ -73,7 +92,7 @@ public class Teste
 					   mudarCor++;
 				   }
 			   }
-			   if(mudarCor != 0){
+			   if(mudarCor != 0 && j > 25 && j < (m1.rows() - 25)){
 				   m1.put(j, i, m2.get(j, i));
 			   }else{
 				   double[] d = {0,0,0};
